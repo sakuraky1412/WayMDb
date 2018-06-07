4654ce11dff671beef3eb62e6335a4597e5d0281
Kuan-Chi Chen <24214304+sakuraky1412@users.noreply.github.com>
2018-06-07 14:27:58 -0400

    Initial Commit

---
+++ WayMDb/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WayMDb
+//
+//  Created by Kuan-Chi Chen on 6/7/18.
+//  Copyright © 2018 Kuan-Chi Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
