55f6e6bb3c90d9d82ae609b574a922c428d39e87
arc4Randall <7901383+arc4randall@users.noreply.github.com>
2018-06-12 11:45:54 -0400

    Revert "Created browse view"

---
+++ WayMDb/TableViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  WayMDb
+//
+//  Created by Kuan-Chi Chen on 6/7/18.
+//  Copyright © 2018 Kuan-Chi Chen. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
